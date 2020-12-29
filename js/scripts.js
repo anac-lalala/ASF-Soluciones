c160fb739ed1295e01323ac6e0d927baf725019c
anac-lalala <anaccaicedoc@gmail.com>
2020-12-29 04:09:39 -0500

    publishing home

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+
+const noticiasAside = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < noticiasAside.length; i++) {
+  noticiasAside[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
